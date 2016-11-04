acb0740b1fcbaed5f3809affaceb13eb8f92a715
markfisher <mfisher@pivotal.io>
2016-11-03 21:21:59 -0400

    wip

--- message-dispatcher/src/main/java/io/pivotal/poc/dispatcher/MessageDispatcherApplication.java
+++ message-dispatcher/src/main/java/io/pivotal/poc/dispatcher/MessageDispatcherApplication.java
@@ -22,12 +22,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.binding.BinderAwareChannelResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.StringUtils;
 
 @EnableBinding
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigurationProperties(HeaderMappingConfigurationProperties.class)
 public class MessageDispatcherApplication {
