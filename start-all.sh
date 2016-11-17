e48d7d8095645d9ef6ae1522701b16099dcbb62f
Marius Bogoevici <mbogoevici@pivotal.io>
2016-11-17 12:42:43 -0500

    Fix recoding pid for zipkin-server

--- start-all.sh
+++ start-all.sh
@@ -6,11 +6,11 @@ if [ ! -d logs ]; then
   mkdir logs
 fi
 
-java -jar zipkin-server/target/zipkin-server-$VERSION.jar > logs/zipkin-server 2>&1 &
-echo $! >> logs/pids
+java -jar zipkin-server/target/zipkin-server-$VERSION.jar > logs/zipkin-server.log 2>&1 &
+echo $! > logs/pids
 
 java -jar config-server/target/config-server-$VERSION.jar > logs/config-server.log 2>&1 &
-echo $! > logs/pids
+echo $! >> logs/pids
 
 java -jar eureka-server/target/eureka-server-$VERSION.jar > logs/eureka-server.log 2>&1 &
 echo $! >> logs/pids
